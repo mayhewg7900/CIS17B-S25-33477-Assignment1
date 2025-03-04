8fba1169b2768eb1a617849c29ce1c9cb4f34e67
Matthew <matthewgaramone@gmail.com>
2025-03-03 17:09:30 -0800

    More changes

--- BankSystem.cpp
+++ BankSystem.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 
 using namespace std;
 
@@ -16,25 +17,66 @@ class BankAccount {
         float getBalance() { return balance; }
 };
 
-enum MenuOptions {CREATE, DEPOSIT, WITHDRAW, CHECK};
+enum MenuOptions {CREATE=0, DEPOSIT, WITHDRAW, CHECK};
 
+void displayMenu();
 int getMenuOption();
+bool isAlpha(string &str);
+float stringToFloat(string &str);
+
+
 
 int main() {
+
     int menu_option;
     MenuOptions menuOption;
+
+    BankAccount* bankAccounts[5];
+    int totalAccounts = 0; 
     do {
-        while(menu_option <= 1 || menu_option >= 5) {
+        displayMenu();
+        while(menu_option < 1 || menu_option > 5) {
             menu_option = getMenuOption();
         }
-        menuOption = (MenuOptions)menu_option;
+        menuOption = (MenuOptions)(menu_option-1);
+        string name = "";
+        string deposit = "";
+        float balance = -1.0f;
         switch(menuOption) {
             case CREATE:
-                
+                cout << "Enter your account name: ";
+                cin >> name;
 
+                while(!isAlpha(name)) {
+                    cin >> name;
+                }
+                cout << "Please enter your initial deposit: ";
+                cin >> deposit;
+
+                balance = stringToFloat(deposit);
+                while(balance == -1) {
+                    cin >> deposit;
+                    balance = stringToFloat(deposit);
+                }
+                
+                createAccount(bankAccounts, totalAccounts, name, balance);
+                cout << "\n\nAccount created successfully!";
+                break;
+
+            case DEPOSIT:
+                break;
+            case WITHDRAW:
+                break;
+            case CHECK:
+                break;
         }
-        
     } while(true);
+
+    for(int i = totalAccounts; i >= 0; i--) {
+        delete bankAccounts[i];
+    }
+
+    return 0;
 
 }
 
@@ -59,4 +101,35 @@ int getMenuOption() {
         cout << "Invalid choice, please enter a valid option (1-5)";
         return 0;
     }
+}
+
+bool isAlpha(string &str) {
+    if(str != "") {
+        for(char c: str) {
+            if(!isalpha(c)) {
+                cout << "Invalid choice, please re-enter your name: ";
+                return false;
+            }
+        }
+        return true;
+    }
+    else {
+        cout << "Invalid choice, please re-enter your name: ";
+        return false;
+    }
+}
+
+float stringToFloat(string &str) {
+    try {
+        float value = stof(str);
+        return value;
+    } catch(const invalid_argument &e) {
+        cout << "Invalid amount, please re-enter: ";
+        return -1;
+    }
+}
+
+void createAccount(BankAccount *bankAccount[], int &index, string name, float balance) {
+    bankAccount[index] = new BankAccount(name, balance);
+    index++;
 }
